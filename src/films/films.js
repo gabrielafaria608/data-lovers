81109cb15ba8212f3154b8af0d5a0a0024375e0f
Gabriela <gabriela.faria806@gmail.com>
2023-07-27 21:09:41 -0300

    T003: feat - implemented responsivity to the index page and other minor changes

--- src/films/films.js
+++ src/films/films.js
@@ -10,36 +10,35 @@ getAll().then(data => {
 }).catch(error=>{
     console.error('error in:',error)
 })
+function createFilms(film) {
+  const card = document.createElement("article");
+  card.classList.add("card");
 
-function createFilms(film){
-    const card = document.createElement("article");
-    card.classList.add("card");
-    
-    const figure = document.createElement("figure");
-    figure.classList.add("images");
-    const image = document.createElement("img");
-    image.src = film.poster;
-    figure.appendChild(image);
-    const title = document.createElement("h2");
-    title.textContent = film.title;
-    const description = document.createElement("p");
-    description.textContent = film.description;
-    const date = document.createElement("p");
-    date.textContent = "Release date: "+film.release_date;
-    const director = document.createElement("p");
-    director.textContent = "Director: "+film.director;
-    const producer = document.createElement("p");
-    producer.textContent = "Producer: "+film.producer;
-    const score = document.createElement("p");
-    score.textContent = "Ratings: "+film.rt_score;
+  const figure = document.createElement("figure");
+  figure.classList.add("images");
+  const image = document.createElement("img");
+  image.src = film.poster;
+  figure.appendChild(image);
+  const title = document.createElement("h2");
+  title.textContent = film.title;
+  const description = document.createElement("p");
+  description.textContent = film.description;
+  const date = document.createElement("p");
+  date.textContent = "Release date: " + film.release_date;
+  const director = document.createElement("p");
+  director.textContent = "Director: " + film.director;
+  const producer = document.createElement("p");
+  producer.textContent = "Producer: " + film.producer;
+  const score = document.createElement("p");
+  score.textContent = "Ratings: " + film.rt_score;
 
-    card.appendChild(figure);
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(date);
-    card.appendChild(director);
-    card.appendChild(producer);
-    card.appendChild(score);
+  card.appendChild(figure);
+  card.appendChild(title);
+  card.appendChild(description);
+  card.appendChild(date);
+  card.appendChild(director);
+  card.appendChild(producer);
+  card.appendChild(score);
 
-    return card;
+  return card;
 }
