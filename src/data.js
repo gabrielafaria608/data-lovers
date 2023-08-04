f9f6e948b88cdae68c49dfef2d43d705642b4965
Gabriela <gabriela.faria806@gmail.com>
2023-08-04 12:10:18 -0300

    T004: feat - creation of order functions of films

--- src/data.js
+++ src/data.js
@@ -1,7 +1,57 @@
 const source = String.raw`\data\ghibli\ghibli.json`;
 
-export async function getAll(){
+export async function getFilms(){
   const response = await fetch(source);
   const data = await response.json();
+  const allFilms = Object.values(data.films).map((item)=> item);
+  return allFilms;
+}
+
+export const releaseOrder = (data, order) =>{
+  if(order === "firstReleased"){
+    data.sort((a,b) => {
+      if (a.release_date < b.release_date){
+        return -1
+      }
+    })
+  }else{
+    data.sort((a,b) => {
+      if (a.release_date > b.release_date){
+        return -1;
+      }
+    })
+  }
+  return data;
+}
+export const alphabeticalOrder = (data, order) => {
+  if(order === "alphabeticalAsc") {
+      data.sort((a,b) => {
+        if(a.title > b.title){
+          return 1;
+        }
+        if(a.title < b.title){
+          return -1;
+        }
+        return 0
+      });
+  }else{
+    data.sort((a,b) => {
+      if(a.title > b.title){
+        return -1;
+      }
+      if(a.title < b.title){
+        return 1;
+      }
+      return 0
+    });
+  }
+  return data;
+}
+export const scoreOrder = (data) => {
+    data.sort((a,b) => {
+      if (a.rt_score > b.rt_score){
+        return -1
+      }
+    })
   return data;
 }
--- src/films/films.js
+++ src/films/films.js
@@ -1,15 +1,73 @@
-import { getAll } from "/./data.js";
+import { getFilms, alphabeticalOrder, releaseOrder, scoreOrder } from "/./data.js";
+
 
 const displayFilms = document.getElementById("displayFilms");
-getAll().then(data => {
-    const films = Object.values(data.films).map((item)=> item);
-    films.forEach(film =>{
-        const card = createFilms(film);
-        displayFilms.appendChild(card);
-    })    
-}).catch(error=>{
-    console.error('error in:',error)
-})
+const orderByForm = document.getElementById("orderByForm");
+const getData = async () => {
+    const data = await getFilms();
+    document.addEventListener("load", showInHtml(data));
+
+    {
+        function showOrderedFilmsAlphabet(event){
+            const order = event.target.value;
+            if(orderByForm.elements["orderByAlphabet"]){
+                const newOrder  = alphabeticalOrder(data, order);
+                displayFilms.innerHTML = "";
+                showInHtml(newOrder);
+            }
+        }
+        const radioButtonsOrderAlphabet = orderByForm.elements["orderByAlphabet"];
+        for (const radioButtonOrder of radioButtonsOrderAlphabet){
+            radioButtonOrder.addEventListener("change", showOrderedFilmsAlphabet);
+        }
+        function showOrderedFilmsRelease(event){
+            const order = event.target.value;
+            if(orderByForm.elements["orderByRelease"]){
+                const newOrder = releaseOrder(data, order);
+                displayFilms.innerHTML = "";
+                showInHtml(newOrder);
+            }
+        }
+        const radioButtonsOrderRelease = orderByForm.elements["orderByRelease"];
+        for (const radioButtonOrder of radioButtonsOrderRelease){
+            radioButtonOrder.addEventListener("change", showOrderedFilmsRelease);
+        }
+        function showOrderedFilmsScore(){
+            if(orderByForm.elements["orderByScore"]){
+                const newOrder = scoreOrder(data);
+                displayFilms.innerHTML = "";
+                showInHtml(newOrder);
+            }
+        }
+        const radioButtonOrderScore = orderByForm.elements["orderByScore"];
+        radioButtonOrderScore.addEventListener("change", showOrderedFilmsScore);
+    }
+
+};
+
+
+{
+const showOrderBtn = document.getElementById("showOrderBtn");
+const orderItems = document.getElementById("orderItems");
+const showFilterBtn = document.getElementById("showFilterBtn")
+const filterItems = document.getElementById("filterItems");
+showOrderBtn.addEventListener("mousedown", () => {
+    if(orderItems.style.display === "none"){
+        orderItems.style.display = "block";
+    }
+    else{
+        orderItems.style.display = "none";
+    }
+});
+showFilterBtn.addEventListener("mousedown", () => {
+    if(filterItems.style.display === "none"){
+        filterItems.style.display = "block";
+    }
+    else{
+        filterItems.style.display = "none";
+    }
+});
+}
 function createFilms(film) {
   const card = document.createElement("article");
   card.classList.add("card");
@@ -21,19 +79,25 @@ function createFilms(film) {
   figure.appendChild(image);
   const title = document.createElement("h2");
   title.textContent = film.title;
+  title.classList.add("cardItems");
   const description = document.createElement("p");
   description.textContent = film.description;
+  description.classList.add("cardItems");
   const date = document.createElement("p");
   date.textContent = "Release date: " + film.release_date;
+  date.classList.add("cardItems");
   const director = document.createElement("p");
   director.textContent = "Director: " + film.director;
+  director.classList.add("cardItems");
   const producer = document.createElement("p");
   producer.textContent = "Producer: " + film.producer;
+  producer.classList.add("cardItems");
   const score = document.createElement("p");
   score.textContent = "Ratings: " + film.rt_score;
+  score.classList.add("cardItems");
 
-  card.appendChild(figure);
   card.appendChild(title);
+  card.appendChild(figure);
   card.appendChild(description);
   card.appendChild(date);
   card.appendChild(director);
@@ -42,3 +106,12 @@ function createFilms(film) {
 
   return card;
 }
+function showInHtml(films){
+    films.forEach(film =>{
+      const card = createFilms(film);
+      displayFilms.appendChild(card);
+    })
+}
+getData();
+
+
